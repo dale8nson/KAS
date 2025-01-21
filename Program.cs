0896f9dbfb71ff0cb69ad716bd95c00ee45ab7c5
Dale Hutchinson <daletristanhutchinson@gmail.com>
2025-01-21 14:26:08 +1100

    Initial project setup with ASP.NET Core and Next.js, including configuration files, models, and basic structure.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using KAS.Models;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddDbContext<GeneralLedgerContext>(opt =>
+{
+    opt.UseSqlServer();
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi("/openapi/{documentName}/openapi.json");
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
